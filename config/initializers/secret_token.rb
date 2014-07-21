4f31c7970bec6393ba966e933b19c3e065265f46
aniketkumar <aniketkumar0077@gmail.com>
2014-07-21 23:18:27 +0530

    Initialization git ststus

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FifthApp::Application.config.secret_key_base = 'e7afa748068c381e841372a58b51f29ebfb1292fb869585b380897184d34a14fa54e170bfe9c04c764c41fc15ebf943c3016a648534631200909e20d154a39f0'
